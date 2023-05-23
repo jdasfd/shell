115c3901944cecc67a6f9264b0e1b5a100f049ab
Yuqian Jiang <597174820@qq.com>
2023-05-23 10:13:41 +0800

    Create git.sh
    add my own git clone

---
+++ git.sh
@@ -0,0 +1,18 @@
+# Clone or pull other repos
+for OP in dotfiles withncbi; do
+    if [[ ! -d "$HOME/Scripts/$OP/.git" ]]; then
+        if [[ ! -d "$HOME/Scripts/$OP" ]]; then
+            echo "==> Clone $OP"
+            git clone https://github.com/wang-q/${OP}.git "$HOME/Scripts/$OP"
+        else
+            echo "==> $OP exists"
+        fi
+    else
+        echo "==> Pull $OP"
+        pushd "$HOME/Scripts/$OP" > /dev/null
+        git pull
+        popd > /dev/null
+    fi
+done
+
+git clone https://github.com/jdasfd/shell.git "$HOME/Scripts/shell"
